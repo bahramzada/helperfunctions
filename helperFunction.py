5a835dcedfcf5b4d92080ed2b2543ed9ce70b18d
Raul Bahramzada <bahramzada.raul.elmaddin.2020@unec.edu.az>
2025-07-07 00:37:40 +0400

    Update helperFunction.py

--- helperFunction.py
+++ helperFunction.py
@@ -2,10 +2,11 @@ import zipfile
 import os
 
 def unzipFile(file_path, target_folder):
-    # Hedef klasör yoksa oluştur
+    # TargetFolder yoxdursa yarat
     if not os.path.exists(target_folder):
         os.makedirs(target_folder)
     with zipfile.ZipFile(file_path, 'r') as zip_ref:
         zip_ref.extractall(target_folder)
     return "Unzip Completed"
+
 
