1dace6622a4a8812bd3aef84dd676149e9b3a9b3
Raul Bahramzada <bahramzada.raul.elmaddin.2020@unec.edu.az>
2025-07-08 12:16:30 +0400

    Update helperFunction.py

--- helperFunction.py
+++ helperFunction.py
@@ -15,19 +15,20 @@ from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 import numpy as np
 
-def calculateMeanStd(data_dir, batch_size=32, num_workers=2):
+def calculateMeanStd(data_dir, batch_size=32, num_workers=2, image_size=(224, 224)):
     transform = transforms.Compose([
+        transforms.Resize(image_size), # Add resizing here
         transforms.ToTensor()
     ])
     dataset = datasets.ImageFolder(data_dir, transform=transform)
     loader = DataLoader(dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
-    
+
     mean = 0.
     std = 0.
     total_images_count = 0
 
     for images, _ in loader:
-        batch_samples = images.size(0)  
+        batch_samples = images.size(0)
         images = images.view(batch_samples, images.size(1), -1)  # (B, C, H*W)
         mean += images.mean(2).sum(0)
         std += images.std(2).sum(0)
@@ -36,5 +37,4 @@ def calculateMeanStd(data_dir, batch_size=32, num_workers=2):
     mean /= total_images_count
     std /= total_images_count
 
-    return mean.numpy(), std.numpy()
-
+    return mean , std
